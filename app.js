d07126af147c32284535f1786dfbc4d13e3fc963
alamrb <alamrb2023@gmail.com>
2024-11-13 23:06:50 +0600

    event start

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { error } = require("console");
 const { readFile } = require("fs");
 
+// 3.17 starting next time
+// Event driven
+
 const getText = (path) => {
   return new Promise((resolve, reject) => {
     readFile(path, "utf8", (error, data) => {
@@ -13,6 +16,13 @@ const getText = (path) => {
   });
 };
 
-getText("./content/first.txt")
-  .then((content) => console.log(content))
-  .catch((error) => console.log(error));
+// getText("./content/first.txt")
+//   .then((content) => console.log(content))
+//   .catch((error) => console.log(error));
+
+const start = async () => {
+  const first = await getText("./content/first.txt");
+  console.log(first);
+};
+
+start();
